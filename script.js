384db28895fb5da91874d0d02c042e7ba50221ec
Alex Davies <alex@mac.home>
2026-02-09 18:54:54 +0000

    Basic logic for calculating interest due based on tax status

---
+++ script.js
@@ -0,0 +1,38 @@
+
+function calculateInterest(isIsa, balance, interestRate, taxStatus) {
+
+    let allowance;
+    let taxRate;
+
+    // Establish tax status rules
+    if (taxStatus === "basic") {
+        allowance = 1000;
+        taxRate = 0.20;
+    }
+    if (taxStatus === "higher") {
+        allowance = 500;
+        taxRate = 0.40;
+    }
+    if (taxStatus === "additional") {
+        allowance = 0;
+        taxRate = 0.45;
+    }
+
+
+    if (isIsa) {
+        return balance * interestRate;
+    }
+
+    //If not an ISA, check if interest exceeds allowance to see if tax is owed
+    if (!isIsa && balance * interestRate <= allowance) {
+        return balance * interestRate;
+    }
+    if (!isIsa && balance * interestRate > allowance) {
+        let interest = balance * interestRate;
+        let taxableInterest = interest - allowance;
+        return interest - (taxableInterest * taxRate);
+    }
+}
+
+calculateInterest(false, 20000, 0.06, "basic")
+
